8003b0b710a652386e59cebee74a6c422df2fabd
themichaelsouza <souz2019@pacificu.edu>
2017-06-24 11:37:25 -0700

    Initial Commit
    Initial program loop and a speed test have been added.

---
+++ souza-OBD/settings.py
@@ -0,0 +1,13 @@
+# game options/settings
+TITLE = "souza-OBD"
+WIDTH = 800
+HEIGHT = 480
+FPS = 10
+
+# define colors
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
